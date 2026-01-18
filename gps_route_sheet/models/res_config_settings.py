6117b56c4707c842c5ee0321c2edd9a0f85e97b2
zhatrus <zhatrus@gmail.com>
2026-01-19 01:07:54 +0200

    Change gps_db_port field type from Integer to Char and add ensure_one() to connection test

--- gps_route_sheet/models/res_config_settings.py
+++ gps_route_sheet/models/res_config_settings.py
@@ -6,13 +6,14 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     gps_db_host = fields.Char(config_parameter="gps_route_sheet.db_host")
-    gps_db_port = fields.Integer(config_parameter="gps_route_sheet.db_port", default=5432)
+    gps_db_port = fields.Char(config_parameter="gps_route_sheet.db_port", default="5432")
     gps_db_name = fields.Char(config_parameter="gps_route_sheet.db_name")
     gps_db_user = fields.Char(config_parameter="gps_route_sheet.db_user")
     gps_db_password = fields.Char(config_parameter="gps_route_sheet.db_password")
 
     @api.model
-    def action_test_gps_db_connection(self):
+    def action_test_gps_db_connection(self, *args, **kwargs):
+        self.ensure_one()
         try:
             self.env["gps.db.service"].test_connection()
         except UserError:
